10436e54cb2f31966f5f1f8fb9f13f4168535d52
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2024-03-09 19:55:08 -0300

    Actualización de código

---
+++ AppInscripcion/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppinscripcionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AppInscripcion'
---
+++ AppInscripcion/urls.py
@@ -0,0 +1,8 @@
+# importaciones de django
+from django.urls import path
+
+# importaciones de la aplicacion
+from . import views # Importamos las vistas de la aplicacion
+
+urlpatterns = [# Luego agregamos las rutas de las vistas
+]
