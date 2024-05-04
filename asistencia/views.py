730921dbc5cf84a10ddadfe58db87d785767ca18
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2024-05-04 14:48:37 -0300

    Actualizar estilos y plantilla de lista de control de asistencia

--- asistencia/views.py
+++ asistencia/views.py
@@ -246,6 +246,7 @@ class SedeDeleteView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixi
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
         return super().delete(request, *args, **kwargs)
+        
 # Create your views here.
 
 def generar_qr(request):  # genera un qr con la url de la pagina de asistencia
