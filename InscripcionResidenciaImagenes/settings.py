44eede428bcb28c78792462b97a4ff1e43341cbb
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2024-05-05 20:27:30 -0300

    chore: Actualizar configuración de correo electrónico

--- InscripcionResidenciaImagenes/settings.py
+++ InscripcionResidenciaImagenes/settings.py
@@ -94,13 +94,13 @@ LOGIN_REDIRECT_URL = 'home'
 
 # Restablecer contraseña
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'soporte.residentesdm@hotmail.com'
-EMAIL_HOST_PASSWORD = '!FQiN.du9LdjsWf'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.office365.com')
+EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'soporte.residentesdm@hotmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '!FQiN.du9LdjsWf')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
