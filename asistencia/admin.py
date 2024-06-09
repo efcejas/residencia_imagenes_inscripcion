29b4497c76695a769b9a94313642430c178fb65e
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2024-06-09 20:03:13 -0300

    feat: Corregir campo de búsqueda en GruposResidentesAdmin
    Se corrigió el campo de búsqueda en la clase `GruposResidentesAdmin` del archivo `admin.py`. Anteriormente, se estaba utilizando el campo `residente__nombre` para buscar residentes por nombre, pero se cambió a solo `residente` para buscar por el objeto residente en su totalidad. Esto mejora la funcionalidad de búsqueda al permitir buscar residentes por su nombre completo.

--- asistencia/admin.py
+++ asistencia/admin.py
@@ -67,7 +67,7 @@ class SedesAdmin(admin.ModelAdmin):
 
 class GruposResidentesAdmin(admin.ModelAdmin):
     list_display = ('residente', 'residencia', 'año')  # Los campos que quieres mostrar en la lista
-    search_fields = ('residente__nombre', 'residencia', 'año')  # Los campos por los que quieres buscar
+    search_fields = ('residente', 'residencia', 'año')  # Los campos por los que quieres buscar
     list_filter = ('residencia', 'año')  # Los campos por los que quieres filtrar
 
 admin.site.register(Usuario, UsuarioAdmin)
